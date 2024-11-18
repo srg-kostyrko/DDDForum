f8d66009b5d41f4ea1194a859f0d91600b763212
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-11-18 09:20:59 +0200

    install shadcn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import VueRouter from 'unplugin-vue-router/vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,6 +16,11 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
